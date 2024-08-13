d2669632eab102690350de0c7d4aa60aadafc6cc
Jaaz7 <jaazielvale@hotmail.com>
2024-08-13 21:56:42 +0200

    Manage node.js packages

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React, { useEffect, useState } from "react";
 import {
   Button,
@@ -84,6 +82,12 @@ export default function LoginPage() {
       alignItems: "center",
       minHeight: "100vh",
     },
+    svgIcon: {
+      width: "25px", // Adjust the size as needed
+      height: "24px", // Adjust the size as needed
+      fill: token.colorPrimary, // Use theme color or adjust as needed
+      marginRight: "8px", // Adjust spacing between icon and title
+    },
   };
 
   if (loading) {
@@ -95,27 +99,17 @@ export default function LoginPage() {
       <div style={styles.container}>
         <div style={styles.header}>
           <svg
-            width="25"
-            height="24"
-            viewBox="0 0 25 24"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            width="25" // Adjusted size
+            height="24" // Adjusted size
+            fill="currentColor"
+            className="bi bi-buildings"
+            viewBox="0 0 16 16"
+            style={styles.svgIcon} // Apply the styles
           >
-            <rect x="0.464294" width="24" height="24" rx="4.8" fill="#1890FF" />
-            <path
-              d="M14.8643 3.6001H20.8643V9.6001H14.8643V3.6001Z"
-              fill="white"
-            />
-            <path
-              d="M10.0643 9.6001H14.8643V14.4001H10.0643V9.6001Z"
-              fill="white"
-            />
-            <path
-              d="M4.06427 13.2001H11.2643V20.4001H4.06427V13.2001Z"
-              fill="white"
-            />
+            <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022M6 8.694 1 10.36V15h5zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5z"/>
+            <path d="M2 11h1v1H2zm2 0h1v1H4zm-2 2h1v1H2zm2 0h1v1H4zm4-4h1v1H8zm2 0h1v1h-1zm-2 2h1v1H8zm2 0h1v1h-1zm2-2h1v1h-1zm0 2h1v1h-1zM8 7h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zM8 5h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zm0-2h1v1h-1z"/>
           </svg>
-
           <Title style={styles.title}>Sign in</Title>
         </div>
         <Form
